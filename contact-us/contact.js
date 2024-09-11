73647e76f8f31b75d6b25b00521d4913d3db89ff
GurTati <tatiana.gurova.j@gmail.com>
2024-09-11 17:28:57 +0100

    attempt to build modal box

--- contact-us/contact.js
+++ contact-us/contact.js
@@ -6,3 +6,28 @@ function submitForm() {
     alert('Thank you!')
 }
 
+//the Modal Box
+//document.addEventListener('DOMContentLoaded', () => {
+
+    const modal = document.getElementsByClassName("modal");
+    const submitBtn = document.getElementById("submitBtn");
+    const closeBtn = document.getElementsByClassName("close")[0];
+ 
+    // Open the modal when the submit button is clicked
+    submitBtn.onclick = function() {
+      modal.style.display = 'block'; // Show the modal
+    }
+  
+    // Close the modal when the close button (X) is clicked
+    closeBtn.onclick = function() {
+      modal.style.display = 'none'; // Hide the modal
+    }
+  
+    // Close the modal if the user clicks outside of the modal content
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = 'none'; // Hide the modal
+    }
+    };
+
+// });
