27877c41bf5c02802b8ae6aac82a8a82509a4b5a
GurTati <tatiana.gurova.j@gmail.com>
2024-09-11 22:00:20 +0100

    add modal box with text

--- contact-us/contact.js
+++ contact-us/contact.js
@@ -1,33 +1,37 @@
-var popupForm = document.querySelector('myForm')
-var popuButton = document.querySelector('form-submit')
+//var popupForm = document.querySelector('myForm')
+//var popuButton = document.querySelector('form-submit')
 
-function submitForm() {
-    document.getElementById('myForm').submit();
-    alert('Thank you!')
-}
+//function submitForm() {
+    //document.getElementById('myForm').submit();
+    //alert('Thank you!')
+//}
 
 //the Modal Box
-//document.addEventListener('DOMContentLoaded', () => {
-
-    const modal = document.getElementsByClassName("modal");
-    const submitBtn = document.getElementById("submitBtn");
-    const closeBtn = document.getElementsByClassName("close")[0];
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('.form-group-container');
+    const openBtn = document.getElementById("submitBtn");
+    const modal = document.querySelector(".modal");
+    const closeBtn = document.querySelector(".close");
  
-    // Open the modal when the submit button is clicked
-    submitBtn.onclick = function() {
-      modal.style.display = 'block'; // Show the modal
-    }
-  
-    // Close the modal when the close button (X) is clicked
-    closeBtn.onclick = function() {
-      modal.style.display = 'none'; // Hide the modal
-    }
-  
-    // Close the modal if the user clicks outside of the modal content
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = 'none'; // Hide the modal
-    }
+    function closeModal(e, clickedOutside) {
+     if (clickedOutside) {
+         if (e.target.classList.contains("modal"))
+             modal.classList.add("hide");
+      } else modal.classList.add("hide");
     };
-
-// });
+ 
+ // When the user clicks the button, open the modal 
+    openBtn.addEventListener("click", function() {
+         modal.classList.remove("hide");
+    });
+ 
+    modal.addEventListener("click", (e) => {
+       closeModal(e, true);
+       location.reload();
+    });
+    closeBtn.addEventListener("click", () => {
+       closeModal;
+       location.reload(); 
+    });
+           
+ });
